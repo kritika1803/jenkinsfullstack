c2478183f8f6d88958f5723d77e5868c0568a125
kritika1803 <chkasya2003@gmail.com>
2025-10-18 02:03:03 +0530

    updated

--- reactfrontend/src/App.jsx
+++ reactfrontend/src/App.jsx
@@ -5,7 +5,7 @@ import TaskBoard from "./components/TaskBoard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/reactfrontendapi">
       <Navbar />
       <div style={{ padding: "20px" }}>
         <Routes>
